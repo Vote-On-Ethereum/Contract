e27f4304fde4246bdcbf47f2380d875b935ad3be
Nicolas Mahé <nicolas@mahe.me>
2017-02-21 10:19:39 +0700

    Add deployment code on live network

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-var Migrations = artifacts.require("./Migrations.sol");
+// var Migrations = artifacts.require("./Migrations.sol");
+var Poll = artifacts.require("./Poll.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  //deployer.deploy(Migrations);
+  deployer.deploy(Poll, "Who is the BEST?");
 };
--- truffle.js
+++ truffle.js
@@ -1,9 +1,33 @@
+'use strict';
+
+const SignerProvider = require('ethjs-provider-signer');
+const ethSigner = require('ethjs-signer')
+
+let liveProviderURL = "http://.......:8545"
+
+let provider = new SignerProvider(liveProviderURL, {
+  signTransaction: (rawTx, cb) => {
+    // console.log(rawTx)
+    cb(null, ethSigner.sign(rawTx, '0x8........'))
+  }
+});
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    live: {
+      host: "......",
+      port: 8545,
+      network_id: 1,        // Ethereum public network
+      // optional config values
+      gas: 280000,
+      // gasPrice
+      from: "0x0.........",// - default address to use for any transaction Truffle makes during migrations
+      provider: provider
     }
   }
 };
